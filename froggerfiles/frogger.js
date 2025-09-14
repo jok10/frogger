06bbe74eceface373e215581e4020d5c6c896d2d
Jok90 <jok10@hi.is>
2025-09-14 22:27:11 +0000

    laga hraða meira

--- froggerfiles/frogger.js
+++ froggerfiles/frogger.js
@@ -14,7 +14,7 @@ const MAX_SCORE = 10;
 const CAR_W = 0.18;
 const CAR_H = 0.22;
 
-const SPEED_SCALE = 0.55; // <<--- change this to slow/speed everything globally
+const SPEED_SCALE = 0.32; 
 
 var canvas, gl, vertices, bufferId, vPosition, colorLoc;
 var lane0Verts, lane0Buffer, lane1Verts, lane1Buffer, lane2Verts, lane2Buffer;
